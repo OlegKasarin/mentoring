08b06b69f09e954de52ccbd6cf9587d8e41d9f92
Oleg Kasarin <Oleg_Kasarin@epam.com>
2016-11-01 22:00:52 +0300

    CarTableVC has been added

---
+++ MentoringApp/CarCell.h
@@ -0,0 +1,15 @@
+//
+//  CarCell.h
+//  MentoringApp
+//
+//  Created by apple on 01.11.16.
+//  Copyright © 2016 OlegKasarin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CarCell : UITableViewCell
+
+@property (weak, nonatomic) IBOutlet UILabel *carLabel;
+@property (weak, nonatomic) IBOutlet UIButton *infoButton;
+@end
---
+++ MentoringApp/CarsTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  CarsTableViewController.h
+//  MentoringApp
+//
+//  Created by apple on 01.11.16.
+//  Copyright © 2016 OlegKasarin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CarsTableViewController : UITableViewController
+
+@end
